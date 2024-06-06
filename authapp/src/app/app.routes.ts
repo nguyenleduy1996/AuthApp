28682663d5150a1c8775803bbddb815b8e07c9ab
nguyenleduy1996 <104442210+nguyenleduy1996@users.noreply.github.com>
2024-06-06 23:54:59 +0700

    Register

--- authapp/src/app/app.routes.ts
+++ authapp/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { LoginComponent } from './component/login/login.component';
+import { ConfirmotpComponent } from './component/confirmotp/confirmotp.component';
+import { RegisterComponent } from './component/register/RegisterComponent';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:'', component: HomeComponent},
+    {path:'register', component: RegisterComponent},
+    {path:'login', component: LoginComponent},
+    {path:'comfirmotp', component: ConfirmotpComponent},
+    
+    
+];
